493559982ddbb028ee2c13525133a714175928f8
mhaouas <mat.haouas@gmail.com>
2024-12-11 12:57:14 +0100

    Updating from vogsphere

--- srcs/requirements/WordPress/tools/start_service.sh
+++ srcs/requirements/WordPress/tools/start_service.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+# Wait until mariadb is set
 until mariadb-admin --host=mariadb ping; do
     sleep 5
 done
 
+# Configure WordPress if it isn't already set
 if [ ! -f /var/www/html/wp-config.php ]; then
     cd  /var/www/html/
     if [ ! -f index.php ]; then
         wp-cli.phar core download
     fi
-    wp-cli.phar config create   --dbname="$MARIADB_DATABASE" --dbuser="$MARIADB_USER" --dbpass="$MARIADB_PASSWORD" --dbhost=mariadb
-    wp-cli.phar core install    --url="mhaouas.42.fr" --title="mhaouas-Inception" --admin_user="$ADMIN_USR" --admin_password="$ADMIN_PWD" --admin_email="$ADMIN_MAIL"
-    wp-cli.phar user create     "$USER_USR" "$USER_MAIL" --role=author --user_pass="$USER_PWD"
+	wp-cli.phar config create   --dbname="$MARIADB_DATABASE" --dbuser="$MARIADB_USER" --dbpass="$MARIADB_PASSWORD" --dbhost=mariadb
+	wp-cli.phar core install    --url="mhaouas.42.fr" --title="mhaouas-Inception" --admin_user="$ADMIN_USR" --admin_password="$ADMIN_PWD" --admin_email="$ADMIN_MAIL"
+	wp-cli.phar user create     "$USER_USR" "$USER_MAIL" --role=author --user_pass="$USER_PWD"
     cd /
 fi
 
+# Start PhP
 php-fpm82 -F
